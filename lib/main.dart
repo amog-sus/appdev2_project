0ae572b02ae8196b9b4ef5cd269e67edd6c95684
Evan <93947013+amog-sus@users.noreply.github.com>
2023-05-03 23:53:31 -0400

    some user pages

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,233 @@ class ExploreEurope extends StatelessWidget {
     return MaterialApp(
       title: 'Explore Europle',
       theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
+          primarySwatch: Colors.blueGrey,
+          textButtonTheme: TextButtonThemeData(
+              style: TextButton.styleFrom(backgroundColor: Color(0xFF4F6A84))),
+          inputDecorationTheme: InputDecorationTheme(
+            fillColor: Color(0xFF4F6A84),
+            filled: true,
+          )),
+      home: LoginPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: Text("Welcome!"),
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: Image.network(
+                          "https://media.discordapp.net/attachments/1096092608415137792/1103513431174299728/image_1.png")
+                      .image)),
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.6,
+                ),
+                TextButton(
+                    onPressed: () {
+                      return;
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF4F6A84)),
+                    child: Text(
+                      "Continue Your Adventure!",
+                      style: TextStyle(color: Colors.white60),
+                    )),
+                TextButton(
+                    onPressed: () {
+                      return;
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF4F6A84)),
+                    child: Text(
+                      "Start Your Adventure!",
+                      style: TextStyle(color: Colors.white60),
+                    )),
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: Text("Log in"),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.fill,
+                image: Image.network(
+                        "https://media.discordapp.net/attachments/1096092608415137792/1103513431174299728/image_1.png")
+                    .image),
+          ),
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.08,
+                ),
+                TextField(
+                    controller: usernameController,
+                    decoration: InputDecoration(
+                      hintText: "Enter Username",
+                    )),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.03,
+                ),
+                TextField(
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                    )),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.1,
+                ),
+                TextButton(
+                    onPressed: () {
+                      return;
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF4F6A84),
+                    ),
+                    child: Text("Register")),
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController fNameController = TextEditingController();
+  TextEditingController lNameController = TextEditingController();
+  TextEditingController bankController = TextEditingController();
+  TextEditingController accNumController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: Text("Register"),
       ),
-      home: ProfilePage(),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.fill,
+                image: Image.network(
+                        "https://media.discordapp.net/attachments/1096092608415137792/1103513431174299728/image_1.png")
+                    .image)),
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.08,
+              ),
+              TextField(
+                  controller: usernameController,
+                  decoration: InputDecoration(
+                    hintText: "Enter Username",
+                  )),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.03,
+              ),
+              TextField(
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
+                  )),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.08,
+              ),
+              TextField(
+                  controller: fNameController,
+                  decoration: InputDecoration(
+                    hintText: "Enter First Name",
+                  )),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.03,
+              ),
+              TextField(
+                  controller: lNameController,
+                  decoration: InputDecoration(
+                    hintText: "Enter Last Name",
+                  )),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.08,
+              ),
+              TextField(
+                  controller: bankController,
+                  decoration: InputDecoration(
+                    hintText: "Your Bank",
+                  )),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.03,
+              ),
+              TextField(
+                  controller: accNumController,
+                  decoration: InputDecoration(
+                    hintText: "Account Number",
+                  )),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.03,
+              ),
+              TextButton(
+                  onPressed: () {
+                    return;
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF4F6A84),
+                  ),
+                  child: Text("Register")),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
