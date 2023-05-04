f55c87e0e47393d2d45652f2d29792097b50837b
Eclipse310 <123961806+Eclipse310@users.noreply.github.com>
2023-05-03 22:46:05 -0400

    Added Basic Files
    Added User Object, passport Object and DBHelper Object.

---
+++ lib/dbhelper.dart
@@ -0,0 +1,77 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import './user.dart';
+
+class DatabaseHelper {
+
+  static final _databaseName = "ExploreEuropeDB.db";
+  static final _databaseVersion = 1;
+
+  static final users_table = 'names_table';
+
+  static final columnId = 'user_id';
+  static final columnFirstName = 'first_name';
+  static final columnLastName = 'last_name';
+  static final columnUsername = 'username';
+  static final columnPassword = 'password';
+  static final columnBankName = 'bank_name';
+  static final columnAccountNum = 'account_num';
+  static final columnPassport = 'passport';
+
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  static Database? _database;
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  _initDatabase() async {
+    String path = join(await getDatabasesPath(), _databaseName);
+    return await openDatabase(path,
+        version: _databaseVersion,
+        onCreate: _onCreate);
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+          CREATE TABLE $users_table (
+            $columnId INTEGER PRIMARY KEY AUTOINCREMENT,
+            $columnFirstName TEXT NOT NULL,
+            $columnLastName TEXT NOT NULL,
+            $columnUsername TEXT NOT NULL,
+            $columnPassword TEXT NOT NULL,
+            $columnBankName TEXT NOT NULL,
+            $columnAccountNum TEXT NOT NULL,
+            $columnPassport TEXT NOT NULL
+          )
+          ''');
+  }
+
+  // Inserts user
+  Future<int> insert(User user) async {
+    Database db = await instance.database;
+    return await db.insert(users_table, {'first_name': user.first_name, 'last_name': user.last_name});
+  }
+
+  // Count rows
+  Future<int?> queryRowCount() async {
+    Database db = await instance.database;
+    return Sqflite.firstIntValue(await db.rawQuery('SELECT COUNT(*) FROM $users_table'));
+  }
+
+  // Update user
+  Future<int> update(User user) async {
+    Database db = await instance.database;
+    int id = user.toMap()['id'];
+    return await db.update(users_table, user.toMap(), where: '$columnId = ?', whereArgs: [id]);
+  }
+
+  // Delete user
+  Future<int> delete(int user_id) async {
+    Database db = await instance.database;
+    return await db.delete(users_table, where: '$columnId = ?', whereArgs: [user_id]);
+  }
+}
---
+++ lib/user.dart
@@ -0,0 +1,39 @@
+import 'dbhelper.dart';
+import 'passport.dart';
+
+class User {
+  int? user_id;
+  String? first_name;
+  String? last_name;
+  String? username;
+  String? password;
+  String? bank_name;
+  String? account_num;
+  Passport? passport;
+
+  User(this.user_id, this.first_name, this.last_name, this.username, this.password, this.bank_name, this.account_num, this.passport);
+
+  User.fromMap(Map<String, dynamic> map) {
+    user_id = map['user_id'];
+    first_name = map['first_name'];
+    last_name = map['last_name'];
+    username = map['username'];
+    password = map['password'];
+    bank_name = map['bank_name'];
+    account_num = map['account_num'];
+    passport = map['passport'];
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      DatabaseHelper.columnId: user_id,
+      DatabaseHelper.columnFirstName: first_name,
+      DatabaseHelper.columnLastName: last_name,
+      DatabaseHelper.columnUsername: username,
+      DatabaseHelper.columnPassword: password,
+      DatabaseHelper.columnBankName: bank_name,
+      DatabaseHelper.columnAccountNum: account_num,
+      DatabaseHelper.columnPassport: passport,
+    };
+  }
+}
